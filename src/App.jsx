3e3723b6f974ff1d3fcf2f8fe619c1e9bd80c340
Arshpreet Singh <loloflol1223@gmail.com>
2024-09-28 11:50:11 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css"
+
+function App() {
+
+    return (
+        <>
+            <h1 className="bg-red-900 flex justify-center p-7 text-lg">Welcome to React!</h1>
+        </>
+    )
+}
+
+export default App;
